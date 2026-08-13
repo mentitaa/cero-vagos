9d3ce1e08863f3073c2c3aadeea4768bf65f3c12
Cero Vagos <info@cerovagos.com>
2026-08-13 22:40:51 +0000

    Sitio regenerado

--- datos/ofertas.js
+++ datos/ofertas.js
@@ -1,6 +1,6 @@
 /* Generado por el motor de Cero Vagos. No editar a mano. */
 window.CERO_VAGOS = {
- "generado": "2026-08-13T22:38:53",
+ "generado": "2026-08-13T22:40:50",
  "total": 281,
  "stats": {
   "total_procesadas": 5433,
