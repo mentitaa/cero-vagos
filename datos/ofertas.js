13c794fbdc72aa5f067a5bd8ccb35ff450718816
Cero Vagos <info@cerovagos.com>
2026-08-13 23:00:20 +0000

    Ofertas del 13/08/2026

--- datos/ofertas.js
+++ datos/ofertas.js
@@ -1,6 +1,6 @@
 /* Generado por el motor de Cero Vagos. No editar a mano. */
 window.CERO_VAGOS = {
- "generado": "2026-08-13T22:48:43",
+ "generado": "2026-08-13T23:00:18",
  "total": 281,
  "stats": {
   "total_procesadas": 5433,
